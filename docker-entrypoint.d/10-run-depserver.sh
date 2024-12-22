f4cc14b334cae9f6e98e795538e41ad5c74c7118
Niels Højen <niels@hojen.net>
2024-12-23 00:57:48 +0100

    Update 10-run-depserver.sh

--- docker-entrypoint.d/10-run-depserver.sh
+++ docker-entrypoint.d/10-run-depserver.sh
@@ -5,11 +5,11 @@
 execServe="/usr/local/bin/depserver"
 
 # API Key - Required
-if [[ ! ${API_KEY} ]]; then
+if [[ ! ${DEP_API_KEY} ]]; then
   /bin/echo "No API Key Set - Exiting"
   exit 0
 else
-  execServe="${execServe} -api ${API_KEY}"
+  execServe="${execServe} -api ${DEP_API_KEY}"
 fi
 
 # Set debug
@@ -18,11 +18,11 @@ if [[ "${DEBUG}" = "true" ]]; then
 fi
 
 # DSN - Required
-if [[ ! ${DBUSER} ]] && [[ ! ${DBPASS} ]] && [[ ! ${DBNAME} ]]; then
+if [[ ! ${DEP_DBUSER} ]] && [[ ! ${DEP_DBPASS} ]] && [[ ! ${DEP_DBNAME} ]]; then
   /bin/echo "Cannot set DSN - Exiting"
   exit 1
 else
-  execServe="${execServe} -storage mysql -storage-dsn '${DBUSER}:${DBPASS}@tcp(${DBHOST:=127.0.0.1}:${DBPORT:=3306})/${DBNAME}'"
+  execServe="${execServe} -storage mysql -storage-dsn '${DEP_DBUSER}:${DEP_DBPASS}@tcp(${DEP_DBHOST:=127.0.0.1}:${DEP_DBPORT:=3306})/${DEP_DBNAME}'"
 fi
 
 # Port to listen on (default ":9001")
